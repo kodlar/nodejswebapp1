26cc511e0fa02bd5d05ca31c13e99e39030c76fd
oğan keskiner work pc <ogankeskiner@gmail.com>
2016-05-26 12:04:20 +0300

    Add project files.

---
+++ NodejsWebApp1/test/time.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+var assert = require('assert');
+var Time = require('../time');
+
+
+describe('time', function () {
+    
+    describe('#constructor', function () {
+        
+        it('Object initializes hour, minute, and AM/PM', function () {
+            
+            var time = new Time();
+            
+            assert(time.hour);
+            assert(time.min);
+            assert(time.ampm);
+        });
+
+        it('Call GetPretty', function () {
+            
+            var time = new Time();
+            
+            assert(time.getPretty());
+            
+            
+        });
+    });
+});
